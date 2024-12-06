fa26d80a654733b1f74faa878d2920f3e9ce1a58
saurabhsquadra <saurabh@squadramedia.com>
2024-12-06 16:07:55 +0530

    initial commit

---
+++ src/api/customer-journey/services/customer-journey.ts
@@ -0,0 +1,7 @@
+/**
+ * customer-journey service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::customer-journey.customer-journey');
---
+++ src/api/dashboard-team-section/controllers/dashboard-team-section.ts
@@ -0,0 +1,7 @@
+/**
+ * dashboard-team-section controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::dashboard-team-section.dashboard-team-section');
